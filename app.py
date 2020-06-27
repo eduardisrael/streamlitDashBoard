5d8a7e4c3829414a1e92ebbbc33dab37867e2897
eduardisrael <israel_pasaca@icloud.com>
2020-06-27 04:21:27 +0000

    fix, dataframe interactivo

--- app.py
+++ app.py
@@ -8,11 +8,14 @@ st.sidebar.title("Analisis de Sentimientos de los tweets sobre las Aerolineas")
 st.markdown("Esta App es un Streamlit Dashboard para analisis los sentimientos de los tweets 🐦")
 st.sidebar.markdown("Esta App es un Streamlit Dashboard para analisis los sentimientos de los tweets 🐦")
 
+DATA_URL = ("/home/rhyme/Desktop/Project/Tweets.csv")
+
 @st.cache(persist=True) #Persiste en cache para optimizar, solo si cambia ejecuta
-DATA_URL = ("/home/rhyme/Desktop/Projects/Tweets.csv")
 def cargar_datos():
     data = pd.read_csv(DATA_URL)
-    data.['teet_created'] = pd.to_datetime(data['tweet_created']) #standard date Pandas
+    data['tweet_created'] = pd.to_datetime(data['tweet_created']) #standard date Pandas
     return data
 
 data = cargar_datos()
+
+st.write(data)
